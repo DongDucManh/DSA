6079c0e3216e9ef20a4e97430b66b0e2b6289734
DongDucManh <manh233630661@lms.utc.edu.vn>
2024-10-23 18:13:45 +0700

    update

--- BTL/Bai 16/graph.cpp
+++ BTL/Bai 16/graph.cpp
@@ -1,18 +1,31 @@
 #ifdef Graph_cpp
 #define Graph_cpp
 #include <bits/stdc++.h>
+#include "linked_list.cpp"
 using namespace std;
-#define int long long
-#define main signed main
 #define elif else if
-#define fix_(x) setprecision(x)<<fixed
 template<class data>
 class Graph{
     private:
-        list<data> a[100000];
+        unordered_map<data, linked_list<data>> graph;
     public:
-        void ipfile(){
-            
+        void addVertex(data vertex){
+            if (graph.find(vertex) == graph.end()){
+                graph[vertex] = linked_list<data>();
+            }
+        }
+        void addEdge(data u, data v){
+            addVertex(u);
+            addVertex(v);
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }   
+        void dfs(){
+            stack<data> s;
+            unordered_map<data, bool> visited;
+            for(auto i: graph){
+                visited[i.first] = false;
+            }
         }
 };
 #endif
